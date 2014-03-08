bc0816f88d2cd4d786f2f76ebb3b6edf6ac687f5
Darron Froese <darron@froese.org>
2014-03-08 09:59:12 -0700

    Adding basic /hit/:page_name controller.

---
+++ app/controllers/page.rb
@@ -0,0 +1,23 @@
+Canary::App.controllers :page do
+
+  get :index, :map => '/hit/:page_name' do
+    @page_name = params[:page_name]
+    render 'index'
+  end
+
+  # get :sample, :map => '/sample/url', :provides => [:any, :js] do
+  #   case content_type
+  #     when :js then ...
+  #     else ...
+  # end
+
+  # get :foo, :with => :id do
+  #   'Maps to url '/foo/#{params[:id]}''
+  # end
+
+  # get '/example' do
+  #   'Hello world!'
+  # end
+
+
+end
