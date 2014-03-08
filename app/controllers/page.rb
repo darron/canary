6bd4fc416fe3053d8d4548a1ca0300423aacb722
Darron Froese <darron@froese.org>
2014-03-08 16:40:53 -0700

    Better way to get request object and use ip instead of REMOTE_ADDR

--- app/controllers/page.rb
+++ app/controllers/page.rb
@@ -2,7 +2,7 @@ Canary::App.controllers :page do
 
   get :index, :map => "/#{ENV['BASE_CANARY_PATH']}/:page_name" do
     @page_name = params[:page_name]
-    info = request.env
+    info = request
     deliver(:hit_notifier, :hit_email, "#{@page_name} was Hit", ENV['EMAIL_DESTINATION'], info) unless ENV['STOP_ALL_EMAIL']
     render 'index'
   end
--- app/mailers/hit_notifier.rb
+++ app/mailers/hit_notifier.rb
@@ -44,7 +44,7 @@ Canary::App.mailer :hit_notifier do
     from destination
     to   destination
     subject the_subject
-    locals :user_agent => info['HTTP_USER_AGENT'], :remote_addr => info['REMOTE_ADDR'], :request_uri => info['REQUEST_URI']
+    locals :user_agent => info.env['HTTP_USER_AGENT'], :remote_addr => info.ip, :request_uri => info.env['REQUEST_URI']
     via     :smtp
     content_type 'text/html'
     render  'hit_email'
