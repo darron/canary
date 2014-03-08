ead84f8beba632a955a46389910509bf7f4cd562
Darron Froese <darron@froese.org>
2014-03-08 12:53:57 -0700

    Allow all email to be silenced with STOP_ALL_EMAIL

--- app/controllers/page.rb
+++ app/controllers/page.rb
@@ -2,7 +2,7 @@ Canary::App.controllers :page do
 
   get :index, :map => "/#{ENV['BASE_CANARY_PATH']}/:page_name" do
     @page_name = params[:page_name]
-    deliver(:hit_notifier, :hit_email, "#{@page_name} was Hit", ENV['EMAIL_DESTINATION'])
+    deliver(:hit_notifier, :hit_email, "#{@page_name} was Hit", ENV['EMAIL_DESTINATION']) unless ENV['STOP_ALL_EMAIL']
     render 'index'
   end
 
